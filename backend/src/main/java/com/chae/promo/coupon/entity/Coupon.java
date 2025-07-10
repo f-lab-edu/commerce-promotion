94927a597012c4be1e2f5a64d43b6c4dcdd1008e
eunchae oh <eunchae1083@gmail.com>
2025-07-10 10:50:29 +0900

    [feat] Coupon 관련 엔티티 및 JPA Auditing 설정 추가
    - Coupon, CouponIssue 엔티티 생성
    - BaseTime 엔티티 추가 및 JpaAuditing 활성화
    - CouponRepository, CouponIssueRepository 추가

---
+++ backend/src/main/java/com/chae/promo/coupon/entity/Coupon.java
@@ -0,0 +1,31 @@
+package com.chae.promo.coupon.entity;
+
+import com.chae.promo.common.entity.BaseTime;
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupons")
+@EntityListeners(AuditingEntityListener.class)
+public class Coupon extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private Integer totalQuantity = 0;
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ backend/src/main/java/com/chae/promo/coupon/entity/CouponIssueStatus.java
@@ -0,0 +1,7 @@
+package com.chae.promo.coupon.entity;
+
+public enum CouponIssueStatus {
+    ISSUED, // 발급됨
+    USED,   // 사용됨
+    EXPIRED // 만료됨
+}
