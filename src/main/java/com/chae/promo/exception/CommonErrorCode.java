c95b23cde6a6081b0d65053461397a60a190d10b
eunchae oh <eunchae1083@gmail.com>
2025-07-13 01:51:25 +0900

    [feat] refreshToken, validation 관련 ErrorCode 상수 추가
    - INVALID_REFRESH_TOKEN: Refresh 토큰 형식 오류
    - REFRESH_TOKEN_EXPIRED: Refresh 토큰 만료
    - VALIDATION_FAILED: 요청 파라미터 유효성 검증 실패

--- src/main/java/com/chae/promo/exception/CommonErrorCode.java
+++ src/main/java/com/chae/promo/exception/CommonErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode {
     JWT_EXPIRED(HttpStatus.FORBIDDEN, "JWT_EXPIRED", "토큰이 만료되었습니다."),
     JWT_INVALID(HttpStatus.UNAUTHORIZED, "JWT_INVALID", "토큰이 유효하지 않습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "알 수 없는 오류입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "알 수 없는 오류입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,"INVALID_REFRESH_TOKEN", "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED,"REFRESH_TOKEN_EXPIRED", "리프레시 토큰이 만료되었습니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VALIDATION_FAILED", "입력 값 유효성 검증에 실패했습니다."),
+    ;
 
 
     CommonErrorCode(HttpStatus httpStatus, String code, String message) {
