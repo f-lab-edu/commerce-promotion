f752974419c9fbfeed4298c1a63cbcc3e055205a
eunchae oh <eunchae1083@gmail.com>
2025-08-05 02:11:42 +0900

    [feat] CreatedAtBase 클래스 분리
    - Basetime에 있던 createdAt 필드를 CreatedAtBase 클래스로 분리

--- backend/src/main/java/com/chae/promo/common/entity/CreatedAtBase.java
+++ backend/src/main/java/com/chae/promo/common/entity/CreatedAtBase.java
@@ -2,14 +2,17 @@ package com.chae.promo.common.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class CreatedAtBase {
 
     @CreatedDate
