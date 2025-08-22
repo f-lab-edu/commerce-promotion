1b467ce43fb49ecfbef7623e2ef852f08acb12f0
eunchae oh <eunchae1083@gmail.com>
2025-08-22 17:21:35 +0900

    [chore] @EnableScheduling 활성화

--- backend/src/main/java/com/chae/promo/PromoApplication.java
+++ backend/src/main/java/com/chae/promo/PromoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableRetry
+@EnableScheduling
 @SpringBootApplication
 public class PromoApplication {
 
