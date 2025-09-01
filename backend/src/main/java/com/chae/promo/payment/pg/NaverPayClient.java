fe8ccbeb2d06123c26d7f24df25dbe16ebcb6f3b
eunchae oh <eunchae1083@gmail.com>
2025-09-02 01:12:06 +0900

    [refactor] 불필요한 reserve 메서드 제거

--- backend/src/main/java/com/chae/promo/payment/pg/NaverPayClient.java
+++ backend/src/main/java/com/chae/promo/payment/pg/NaverPayClient.java
@@ -11,8 +11,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -23,7 +21,6 @@ public class NaverPayClient {
     @Value("${naverpay.partner-id}") String partnerId;
     @Value("${naverpay.client-id}") String clientId;
     @Value("${naverpay.client-secret}") String clientSecret;
-//    @Value("${naverpay.return-url}") String returnUrl;
 
     private Consumer<HttpHeaders> auth(String idem) {
         return h -> {
@@ -33,35 +30,7 @@ public class NaverPayClient {
         };
     }
 
-    // 1) 결제예약: reserveId 반환
-    public String reserve(String orderKey, BigDecimal amount, String title, long quantity) {
-        var req = Map.of(
-                "modelVersion", "2",
-                "merchantPayKey", orderKey,
-                "merchantUserKey", "U-" + orderKey,
-                "productName", title,
-                "productCount", quantity,
-                "totalPayAmount", amount
-//                "returnUrl", returnUrl
-        );
-        Map<?,?> resp = client.post()
-                .uri("/" + partnerId + "/naverpay/payments/v2/reserve")
-                .headers(auth(UUID.randomUUID().toString()))
-                .bodyValue(req)
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
-        Map<?,?> body = (Map<?,?>) resp.get("body"); // 기본 응답 포맷 {code,message,body} 구조
-        return (String) body.get("reserveId");
-    }
-
-    // 결제창 URL 구성
-    public String buildPaymentPage(String reserveId, boolean prod) {
-        String host = prod ? "https://m.pay.naver.com" : "https://test-m.pay.naver.com";
-        return host + "/payments/" + reserveId;
-    }
-
-    // 3) 결제승인: paymentId로 승인
+    // 결제승인: paymentId로 승인
     public NaverPayApproveResponse approve(String paymentId) {
         MultiValueMap<String,String> form = new LinkedMultiValueMap<>();
 
